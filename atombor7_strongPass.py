1c1f3227585e12263ad3408e35c1a8f9e5fe6d33
cdelaguardia <carlos.delaguardia@etecsa.cu>
2021-05-13 21:07:57 -0400

    YA se anadio deteccion de caracter especial, todo listo

--- atombor7_strongPass.py
+++ atombor7_strongPass.py
@@ -12,13 +12,18 @@ passNumber = re.compile(r'''(
     [0-9]
     )''', re.VERBOSE)
 
+passChar = re.compile(r'''(
+    [\W]
+    )''', re.VERBOSE)
+
 password =str(pyperclip.paste())
 
 lower=passLowerCase.search(password)
 upper=passUpperCase.search(password)
 number=passNumber.search(password)
+char=passChar.search(password)
 
-if lower !=None and upper!=None and number !=None:
+if lower !=None and upper!=None and number !=None and char != None:
     print('pass segura')
 else:
     print('pass no segura')
