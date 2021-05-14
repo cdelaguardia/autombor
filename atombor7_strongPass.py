7fa172d529dcbf5530a7e07476466751b0ce5665
cdelaguardia <carlos.delaguardia@etecsa.cu>
2021-05-13 20:59:56 -0400

    Ya este codigo funciona, despues quiero anadirle deteccion de caracteres especiales

--- atombor7_strongPass.py
+++ atombor7_strongPass.py
@@ -1,1 +1,24 @@
 import pyperclip, re
+
+passLowerCase = re.compile(r'''(
+    [a-z]
+    )''', re.VERBOSE)
+
+passUpperCase = re.compile(r'''(
+    [A-Z]
+    )''', re.VERBOSE)
+
+passNumber = re.compile(r'''(
+    [0-9]
+    )''', re.VERBOSE)
+
+password =str(pyperclip.paste())
+
+lower=passLowerCase.search(password)
+upper=passUpperCase.search(password)
+number=passNumber.search(password)
+
+if lower !=None and upper!=None and number !=None:
+    print('pass segura')
+else:
+    print('pass no segura')
