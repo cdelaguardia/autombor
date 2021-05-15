83a4539b0f434f5c67a4b05f1db8364613cdc0c8
cdelaguardia <carlos.delaguardia@etecsa.cu>
2021-05-14 21:09:13 -0400

    Se annade la eleccion de la proteina y el calculo de su precio

--- atombor8_sandwish.py
+++ atombor8_sandwish.py
@@ -9,6 +9,13 @@ many=0
 price=0
 
 print('This program will help you to prepare your favorite sandwish')
+
 print('Choose the bread type for your sandwish')
 bread = pyinputplus.inputMenu(bread_type, numbered = True)
 price+=bread_price_list[bread_type.index(bread)]
+
+print('\nChoose the protein type for your sandwish')
+protein = pyinputplus.inputMenu(protein_type, numbered = True)
+price+=protein_price_list[protein_type.index(protein)]
+
+print(price)
