1a0da47a4e5b29a193f3b42d09f6ef9cd13669fb
cdelaguardia <carlos.delaguardia@etecsa.cu>
2021-05-14 21:04:50 -0400

    Se anade escoger el pan y calcular su precio

--- atombor8_sandwish.py
+++ atombor8_sandwish.py
@@ -1,6 +1,14 @@
 import pyinputplus
 
 bread_type = ['wheat', 'white', 'black']
+bread_price_list = [2, 2.5, 1]
 protein_type = ['chicken', 'turkey', 'ham', 'tofu']
+protein_price_list = [5, 5.2, 3, 2]
+cheese_price = 1
+many=0
+price=0
 
-f=pyinputplus.inputMenu()
+print('This program will help you to prepare your favorite sandwish')
+print('Choose the bread type for your sandwish')
+bread = pyinputplus.inputMenu(bread_type, numbered = True)
+price+=bread_price_list[bread_type.index(bread)]
